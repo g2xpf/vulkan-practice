fee50144b3c4f1335becc868903019c2580f5b65
g2_xpf <nameko.jack@gmail.com>
2019-09-04 20:02:54 +0900

    impl: vulkan initialization

--- src/HelloTriangle/main.cpp
+++ src/HelloTriangle/main.cpp
@@ -18,6 +18,7 @@ public:
 
     void run() {
         initWindow();
+        printAvailableExtensions();
         initVulkan();
         mainLoop();
         cleanup();
@@ -25,6 +26,7 @@ public:
 
 private:
     GLFWwindow* window;
+    VkInstance instance;
 
     void initWindow() {
         glfwInit();
@@ -32,13 +34,70 @@ private:
         glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
         window = glfwCreateWindow(WIDTH, HEIGHT, "GLWindow", nullptr, nullptr);
     }
-    void initVulkan() {}
+
+    void initVulkan() {
+        createInstance();
+    }
+
+    void printAvailableExtensions() {
+        // 使用可能な Vulkan Extension の確認
+
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+        std::cout << extensionCount << " available extension(s): " << std::endl;
+
+        for(const auto& extension: extensions) {
+            std::cout << "\t" << extension.extensionName << std::endl;
+        }
+    }
+
+    void createInstance() {
+        // このアプリ自体の設定.
+        // ココで与えるパラメータはオプショナルで,
+        // 最適化に関わったりする.
+        VkApplicationInfo appInfo = {};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        // VkInstance を作成するためのパラメータ.
+        VkInstanceCreateInfo createInfo = {};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        // glfw と連結
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        // VkInstance の作成
+        // 第二引数は custom allocator.
+        if(vkCreateInstance(&createInfo, nullptr, &this->instance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance");
+        }
+    }
+
     void mainLoop() {
         while(!glfwWindowShouldClose(window)) {
             glfwPollEvents();
         }
     }
+    
     void cleanup() {
+        // 第二引数は allocator callback.
+        vkDestroyInstance(instance, nullptr);
+
         glfwDestroyWindow(window);
 
         glfwTerminate();
