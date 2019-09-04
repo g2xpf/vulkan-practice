7118f0af4fb1be8e7906d67a88e960c01f41c04d
g2_xpf <nameko.jack@gmail.com>
2019-09-04 12:18:02 +0900

    Impl: glfw part

--- src/HelloTriangle/main.cpp
+++ src/HelloTriangle/main.cpp
@@ -1,22 +1,48 @@
 #include <vulkan/vulkan.h>
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 #include <stdexcept>
 #include <functional>
 #include  <cstdlib>
 
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
 class HelloTriangleApplication {
 public:
+    HelloTriangleApplication(): window(nullptr) {
+    }
+
     void run() {
+        initWindow();
         initVulkan();
         mainLoop();
         cleanup();
     }
 
 private:
+    GLFWwindow* window;
+
+    void initWindow() {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        window = glfwCreateWindow(WIDTH, HEIGHT, "GLWindow", nullptr, nullptr);
+    }
     void initVulkan() {}
-    void mainLoop() {}
-    void cleanup() {}
+    void mainLoop() {
+        while(!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+    }
+    void cleanup() {
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+    }
 };
 
 int main() {
